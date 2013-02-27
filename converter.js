f82d91acde1ea5db8fa703bc5ddf7f2c2e4b978e
Sean Lawrence <lawrensm@gmail.com>
2013-02-27 10:56:11 -0500

    More updates

--- converter.js
+++ converter.js
@@ -1,16 +1,20 @@
-var CONVERTER = (function (global) {
+var CONVERTER = (function () {
     var dependencies = {},
         ignore = {
             'PTO.app': true,
             'PTO.config': true,
-            'PTO.log': true,
-            'dojo.declare': true
+            'PTO.log': true
         },
         shouldIgnore = function (string) {
             for (var prop in ignore) {
                 if (string.indexOf(prop) === 0) {
                     return ignore[prop];
                 }
+            }
+        },
+        addDependency = function (depend, alias) {
+            if (!dependencies[depend]) {
+                dependencies[depend] = alias;
             }
         },
         upperFirstChar = function (string) { return string.charAt(0).toUpperCase() + string.slice(1); },
@@ -32,122 +36,131 @@ var CONVERTER = (function (global) {
         },
         replacements = [
             {
-                pattern: /dojo\.addClass/g,
+                pattern: /dojo\.(addClass[\w\.]*)/g,
                 depend: 'dojo/dom-class',
             },
             {
-                pattern: /dojo\.attr/g,
+                pattern: /dojo\.(attr[\w\.]*)/g,
                 depend: 'dojo/dom-attr',
             },
             {
-                pattern: /dojo\.byId/g,
+                pattern: /dojo\.(byId[\w\.]*)/g,
                 depend: 'dojo/dom',
             },
             {
-                pattern: /dojo\.connect/g,
+                pattern: /dojo\.(connect[\w\.]*)/g,
                 depend: 'dojo/_base/connect',
             },
             {
-                pattern: /dojo\.create/g,
+                pattern: /dojo\.(create[\w\.]*)/g,
                 depend: 'dojo/dom-construct',
             },
             {
-                pattern: /dojo\.declare/g,
+                pattern: /dojo\.(declare[\w\.]*)/g,
                 depend: 'dojo/_base/declare',
+                alias: 'declare',
+                repFn: function (all) {
+                    //hacky work around specifically for declare, since we don't won't to replace it till the end
+                    addDependency(this.depend, this.alias);
+                    return all;
+                }
             },
             {
-                pattern: /dojo\.destroy/g,
+                pattern: /dojo\.(destroy[\w\.]*)/g,
                 depend: 'dojo/dom-construct',
             },
             {
-                pattern: /dojo\.disconnect/g,
+                pattern: /dojo\.(disconnect[\w\.]*)/g,
                 depend: 'dojo/_base/connect',
             },
             {
-                pattern: /dojo\.empty/g,
+                pattern: /dojo\.(empty[\w\.]*)/g,
                 depend: 'dojo/dom-construct',
             },
             {
-                pattern: /dojo\.forEach/g,
+                pattern: /dojo\.(forEach[\w\.]*)/g,
                 depend: 'dojo/_base/array',
             },
             {
-                pattern: /dojo\.hasClass/g,
+                pattern: /dojo\.(hasClass[\w\.]*)/g,
                 depend: 'dojo/dom-class',
             },
             {
-                pattern: /dojo\.hitch/g,
+                pattern: /dojo\.(hitch[\w\.]*)/g,
                 depend: 'dojo/_base/lang',
             },
             {
-                pattern: /dojo\.keys/g,
+                pattern: /dojo\.(keys[\w\.]*)/g,
                 depend: 'dojo/keys',
             },
             {
-                pattern: /dojo\.map/g,
+                pattern: /dojo\.(map[\w\.]*)/g,
                 depend: 'dojo/_base/array',
             },
             {
-                pattern: /dojo\.mixin/g,
+                pattern: /dojo\.(mixin[\w\.]*)/g,
                 depend: 'dojo/_base/lang',
             },
             {
-                pattern: /dojo\.place/g,
+                pattern: /dojo\.(place[\w\.]*)/g,
                 depend: 'dojo/dom-construct',
             },
             {
-                pattern: /dojo\.query/g,
+                pattern: /dojo\.(query[\w\.]*)/g,
                 depend: 'dojo/query',
             },
             {
-                pattern: /dojo\.removeClass/g,
+                pattern: /dojo\.(removeClass[\w\.]*)/g,
                 depend: 'dojo/dom-class',
             },
             {
-                pattern: /dojo\.replaceClass/g,
+                pattern: /dojo\.(replaceClass[\w\.]*)/g,
                 depend: 'dojo/dom-class',
             },
             {
-                pattern: /dojo\.some/g,
+                pattern: /dojo\.(some[\w\.]*)/g,
                 depend: 'dojo/_base/array',
             },
             {
-                pattern: /dojo\.stopEvent/g,
+                pattern: /dojo\.(stopEvent[\w\.]*)/g,
                 depend: 'dojo/_base/event',
             },
             {
-                pattern: /dojo\.style/g,
+                pattern: /dojo\.(style[\w\.]*)/g,
                 depend: 'dojo/dom-style',
             },
             {
-                pattern: /dojo\.toggleClass/g,
+                pattern: /dojo\.(toggleClass[\w\.]*)/g,
                 depend: 'dojo/dom-class',
             },
             {
-                pattern: /dojo\.window/g,
+                pattern: /dojo\.(window[\w\.]*)/g,
                 depend: 'dojo/_base/window',
             },
             {
-                pattern: /dijit\.byId/g,
+                pattern: /dijit\.(byId[\w\.]*)/g,
                 depend: 'dijit/registry',
             },
             {
                 pattern: /PTO\.([\w\.]+)/g,
                 repFn: function (all, rest) {
-                    var pieces = rest.split('.');
-                    //TODO: We can make assumptions here (I hope), if the last piece is capitalized it's a constructor
-                    if (pieces[pieces.length - 1].charAt(0).match(/[A-Z]/)) {
-                        this.alias = pieces[pieces.length -1];
+                    var pieces = rest.split('.'),
+                        last = pieces[pieces.length - 1];
+                    //We can make assumptions here (I hope), if the last piece is capitalized it's a constructor
+                    if (last.charAt(0).match(/[A-Z]/)) {
+                        this.alias = last;
                         this.depend = all;
                     } else {
                         //it's a method
-                        if (pieces.length > 2) {
-                            this.alias = pieces[pieces.length - 2] + '.' +
-                                                 pieces[pieces.length -1];
-                            this.depend = pieces[pieces.length - 2];
+                        if (pieces.length > 1) {
+                            //piece before last is alias
+                            this.alias = pieces[pieces.length - 2] + "." + last;
+                            //don't include method in dependency
+                            this.depend = all.replace(/\.[\w]+$/, "");
                         } else {
-                            this.alias = rest;
-                            this.depend = pieces[0];
+                            //not sure what to do here, e.g. PTO
+                            this.alias = all;
+                            this.depend = all;
                         }
                     }
                 }
@@ -155,6 +168,12 @@ var CONVERTER = (function (global) {
         ];
 
 
+    /**
+     * Loops through replacements array. If the pattern is found the following steps are executed:
+     * 1. return all (do no replacement) if should be ignored
+     * 2. apply fn if defined
+     * 3. assign dependency with alias to dependencies map
+     */
     function replaceOldDojo(string) {
         var i, replacement;
 
@@ -162,22 +181,26 @@ var CONVERTER = (function (global) {
         for (i = 0; i < replacements.length; i += 1) {
             replacement = replacements[i];
 
-            string = string.replace(replacement.pattern, function (all) {
-                var argArray, alias;
+            string = string.replace(replacement.pattern, function (all, rest) {
+                var argArray, alias, fnReturn;
                 if (shouldIgnore(all)) {
                     return all;
                 }
                 //apply fn if defined
                 if (replacement.repFn) {
                     argArray = Array.prototype.slice.apply(arguments);
-                    replacement.repFn.apply(replacement, argArray);
+                    fnReturn = replacement.repFn.apply(replacement, argArray);
+                    //allow repFn to define replacement directly if it desires
+                    if (typeof fnReturn !== "undefined") {
+                        return fnReturn;
+                    }
                 }
                 //determine alias (a repFn may define one)
                 if (replacement.alias) {
                     alias = replacement.alias;
                 } else {
                     //lookup on dependency map
-                    alias = dependNameMap[replacement.depend];
+                    alias = dependNameMap[replacement.depend] + "." + rest;
                     if (!alias) {
                         console.error("Uh oh. Alias not specified.");
                     }
@@ -189,52 +212,42 @@ var CONVERTER = (function (global) {
         return string;
     }
 
-    //TODO: Refactor!
-    function addDependency(depend, alias) {
-        if (!dependencies[depend]) {
-            dependencies[depend] = alias;
-        }
-        //if (dependencies[depend]) {
-            //if (alias) {
-                //if (dependencies[depend][alias]) {
-                    //dependencies[depend][alias] += 1;
-                //} else {
-                    //dependencies[depend][alias] = 1;
-                //}
-            //}
-        //} else {
-            //dependencies[depend] = {};
-            //if (alias) {
-                //dependencies[depend][alias] = 1;
-            //}
-        //}
-    }
 
+    /**
+     * Basically replaces provide/require with empty strings
+     */
     function convertRequires(string) {
+        var providePattern = /dojo\.provide\((?:'|")[\w\.]+(?:'|")\);/g,
+            requirePattern = /dojo\.require\((?:'|")([\w\.]+)(?:'|")\);/g;
         //replace provide
-        string = string.replace(/dojo\.provide\((?:'|")[\w\.]+(?:'|")\);/g, "");
+        string = string.replace(providePattern, "");
 
         //replace requires, add dependency
-        string = string.replace(/dojo\.require\((?:'|")([\w\.]+)(?:'|")\);/g, function (all, ns) {
+        string = string.replace(requirePattern, function (all, ns) {
             //addDependency(ns); //this shouldn't be necessary
             return "";
         });
         return string;
     }
 
+    /**
+     * Surround declare with define function. At this point we should have a populated dependency map.
+     * NOTE: We can only convert files that have one declare statement!
+     */
     function convertDeclare(string) {
-        return string.replace(/dojo\.declare\((?:'|")([\w\.]+)(?:'|"),\s*(null|[\w\.]+|\[[\w\.\s,]*\])[\S\s]*\}\);/g, function (all, className, parents) {
+        var declarePattern = /dojo\.declare\((?:'|")([\w\.]+)(?:'|"),\s*(null|[\w\.]+|\[[\w\.\s,]*\])[\S\s]*\}\);/g;
+        return string.replace(declarePattern, function (all, className, parents) {
             //remove class name, it's not necessary.
             all = all.replace(/('|")[\w\.]*('|"),\s*/, "");
-            //add return
-            all = "return " + all;
             //surround with define
-            all = defineString(dependencies) + all + "\n});";
+            all = defineString(dependencies) + "var model = " + all + "\nreturn model;\n});";
             return all;
         });
     }
 
-    //creates define statement given dependency object
+    /*
+     * creates define statement given dependency object
+     */
     //TODO: Refactor
     function defineString(dependObject) {
         var str = "",
@@ -259,8 +272,10 @@ var CONVERTER = (function (global) {
 
     return {
         convert: function (fileString) {
-            return convertDeclare(replaceOldDojo(convertRequires(fileString))).trim();
+            fileString = convertDeclare(replaceOldDojo(convertRequires(fileString))).trim();
+            //hacky workaround for declare, since we only want to replace it at the end
+            return fileString.replace(/dojo\.declare/g, "declare");
         },
         dependencies: dependencies
     };
-}(window));
+}());
